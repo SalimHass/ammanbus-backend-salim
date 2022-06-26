44768ac859eb479e912db363549db1ec05f88b89
Salim Hassouneh <salim.hassouneh@gmail.com>
2022-06-26 13:58:50 +0300

    models1

--- driversapp/admin.py
+++ driversapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Station,Route,Bus,StationStop
 # Register your models here.
+admin.site.register(Station)
+admin.site.register(Route)
+admin.site.register(Bus)
+admin.site.register(StationStop)
+
